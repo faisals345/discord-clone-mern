29fc4e2afb4f67a137c5e6e18516fdd975802b78
faisals345 <2019ugcs015@nitjsr.ac.in>
2022-06-05 16:37:10 +0530

    first commit

---
+++ discord-backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Welcome, This App is a Discord clone",
+    });
+  });
+
+server.listen(PORT,()=>console.log( `Server is listening on ${PORT}`));
