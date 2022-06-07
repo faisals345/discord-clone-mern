5dcbcd000a0297881cb0017f3bf15b42e3d5a0b0
faisals345 <2019ugcs015@nitjsr.ac.in>
2022-06-07 16:31:22 +0530

    login and register route configured

--- discord-backend/server.js
+++ discord-backend/server.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const authRoutes = require('./routes/authRoutes');
-
+const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-
-// register 
-app.use('/api/auth/',authRoutes);
-
+// register the routes
+app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-      message: "Welcome, This App is a Discord clone",
-    });
-  });
-
-// server.listen(PORT,()=>console.log( `Server is listening on ${PORT}`));
-
-// console.log(process.env.MONGO_URI);
 mongoose
-  .connect(
-    process.env.MONGO_URI
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT);
-    console.log("success");
-   
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
   })
-  .catch(() => {
-    console.log("Connection failed!");
+  .catch((err) => {
+    console.log("database connection failed. Server not started");
+    console.error(err);
   });
