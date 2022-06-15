1ae32d8835d6644984e40f78c294d4893947fd55
faisals345 <2019ugcs015@nitjsr.ac.in>
2022-06-15 10:11:27 +0530

    dashboard login register ui created

---
+++ discord-frontend/src/Dashboard/AppBar/AppBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { styled } from '@mui/system';
+
+
+const MainContainer = styled('div')({
+
+    position:"absolute",
+    backgroundColor:"#36393f",
+    right:"0",
+    top:"0",
+    height:"48px",
+    borderBottom:"1px solid black",
+    width: 'calc(100% - 326px)',
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"space-between",
+    padding: "0 15px",
+
+});
+const AppBar = ()=>{
+
+    return (
+        <MainContainer>
+
+        </MainContainer>
+    )
+
+};
+
+export default AppBar;
---
+++ discord-frontend/src/Dashboard/Messenger/Messenger.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { styled } from '@mui/system';
+
+
+const MainContainer = styled('div')({
+
+    flexGrow:'1',
+    backgroundColor:'#2F3136',
+    marginTop:"48px",
+    display:'flex',
+});
+const Messenger = ()=>{
+
+    return (
+        <MainContainer>
+
+        </MainContainer>
+    )
+
+};
+
+export default Messenger;
