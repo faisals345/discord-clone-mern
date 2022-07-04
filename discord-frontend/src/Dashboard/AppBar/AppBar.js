a1bf5c790ad15d99f28c0dafd329dc68f6168b09
faisals345 <2019ugcs015@nitjsr.ac.in>
2022-07-04 19:21:03 +0530

    dropdown men u added as well as token clear from local strorage and socket io connected

--- discord-frontend/src/Dashboard/AppBar/AppBar.js
+++ discord-frontend/src/Dashboard/AppBar/AppBar.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { styled } from '@mui/system';
+import React from "react";
+import { styled } from "@mui/system";
+import DropdownMenu from "./DropdownMenu";
 
-
-const MainContainer = styled('div')({
-
-    position:"absolute",
-    backgroundColor:"#36393f",
-    right:"0",
-    top:"0",
-    height:"48px",
-    borderBottom:"1px solid black",
-    width: 'calc(100% - 326px)',
-    display:"flex",
-    alignItems:"center",
-    justifyContent:"space-between",
-    padding: "0 15px",
-
+const MainContainer = styled("div")({
+  position: "absolute",
+  right: "0",
+  top: "0",
+  height: "48px",
+  borderBottom: "1px solid black",
+  backgroundColor: "#36393f",
+  width: "calc(100% - 326px)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "0 15px",
 });
-const AppBar = ()=>{
 
-    return (
-        <MainContainer>
-
-        </MainContainer>
-    )
-
+const AppBar = () => {
+  return (
+    <MainContainer>
+      <DropdownMenu />
+    </MainContainer>
+  );
 };
 
 export default AppBar;
